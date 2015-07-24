599d09a35d2797c6fae5756d973e0fc164db0cd2
scott <scottmacphersonmusic@gmail.com>
2015-07-24 14:27:28 -0700

    Add to_s method to Book

--- library.rb
+++ library.rb
@@ -10,4 +10,15 @@ class Shelf
 end
 
 class Book
+  attr_reader :title, :author, :genre
+
+  def initialize(attr = {})
+    @title     = attr[:title]
+    @author    = attr[:author]
+    @genre     = attr[:genre]
+  end
+
+  def to_s
+    "\"#{@title}\" by #{@author['first_name']} #{@author['last_name']}"
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -3,7 +3,9 @@ require './test/test_helper'
 class BookTest < MiniTest::Test
   include SampleBooks
 
-  def test_book
-    assert Book.new
+  def test_to_s
+    mockingbird = Book.new(SampleBooks::Mockingbird)
+    assert_equal '"To Kill A Mockingbird" by Harper Lee',
+                 mockingbird.to_s
   end
 end
